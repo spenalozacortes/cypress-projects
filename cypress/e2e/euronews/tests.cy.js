3322bae74da96e62689634a536cc025cb068d511
Stephanie Peñaloza <spenalozacortes@gmail.com>
2024-04-11 11:16:45 -0600

    Create page objects for euronews tests

--- cypress/e2e/euronews/tests.cy.js
+++ cypress/e2e/euronews/tests.cy.js
@@ -5,6 +5,11 @@ import testdata from '../../fixtures/testdata.json';
 import utils from '../../support/utils';
 import { euronews } from '../../support/locators';
 import { assertions } from './assertionData';
+import MainPage from '../../pageobjects/euronews/mainPage';
+import TopicPage from '../../pageobjects/euronews/topicPage';
+import ProgramsPage from '../../pageobjects/euronews/programsPage';
+import NewslettersPage from '../../pageobjects/euronews/newslettersPage';
+import RegistrationPage from '../../pageobjects/euronews/registrationPage';
 
 describe('Euronews tests', () => {
     beforeEach(() => {
@@ -13,70 +18,75 @@ describe('Euronews tests', () => {
         cy.visit(urls.euronews);
     });
 
-    it('test case 1', () => {
-        // Main page of Euronews is opened 
+    xit('test case 1', () => {
+        // Main page of Euronews is opened
+        const mainPage = new MainPage(); 
         cy.url().should('eq', assertions.baseUrl);
-        cy.get(euronews.agreeCookiesButton).click();
+        mainPage.agreeCookiesButton.click();
         // Top tags are displayed
-        cy.get(euronews.topTags).should('be.visible');
+        mainPage.topTags.should('be.visible');
         // Views displayed 
-        cy.get(euronews.views).should('be.visible');
+        mainPage.views.should('be.visible');
         // Step 2
         // Open one of Views 
-        cy.get(euronews.viewsTitles).eq(1).click().invoke('text').then(homeTitle => {
+        mainPage.viewTitles.eq(2).click().invoke('text').then(homeTitle => {
             // View is opened and has the correct title (corresponds to the title on the home page) 
-            cy.get(euronews.viewTitle).should('contain.text', homeTitle.trim());
+            mainPage.viewTitle.should('contain.text', homeTitle.trim());
         }); 
         // Step 3
         // Click on “Programmes”
-        cy.get(euronews.programsButton).click();
+        mainPage.programsButton.click();
         // All mandatory programs are on the list: My europe, Sport, World, Next, Travel, Green, Culture, Special coverage, Partner content
-        cy.get(euronews.programsTitles).then($titles => {
+        mainPage.programTitles.then($titles => {
             const titlesStrings = [...$titles].map(el => el.innerText);
             expect(titlesStrings).to.include.members(testdata.programs);
         });
         // All programs have at least 1 topic
-        cy.get(euronews.programsTitles).each($title => {
+        mainPage.programTitles.each($title => {
             cy.wrap($title).next(euronews.topicLinks).should('exist');
         });
         // Step 4
         // Click on any topic in “My Europe”
-        cy.contains(euronews.topic).click();
+        mainPage.topic.click();
         // My Europe page is opened
+        const topicPage = new TopicPage();
         cy.url().should('contain', assertions.myEuropeUrl);
         // Each news item is labeled according to the selected topic
-        cy.get(euronews.labels).each($label => {
+        topicPage.labels.each($label => {
             cy.wrap($label).should('contain.text', testdata.topic);
         });
         // Step 5
         // Click “Programmes” -> “All Programmes”
-        cy.get(euronews.programsDropdown).click();
-        cy.contains(euronews.programsLink).click();
+        topicPage.programsDropdown.click();
+        topicPage.programsLink.click();
         // More than 30 programs are displayed
-        cy.get(euronews.programs).should('have.length.above', 30);
+        const programsPage = new ProgramsPage();
+        programsPage.programs.should('have.length.above', 30);
     });
 
     it('test case 2', () => {
-        // Main page of Euronews is opened 
+        // Main page of Euronews is opened
+        const mainPage = new MainPage(); 
         cy.url().should('eq', assertions.baseUrl);
-        cy.get(euronews.agreeCookiesButton).click();
+        mainPage.agreeCookiesButton.click();
         // Top tags are displayed
-        cy.get(euronews.topTags).should('be.visible');
+        mainPage.topTags.should('be.visible');
         // Step 2
         // Follow the link "Newsletters" in the header 
-        cy.contains(euronews.newslettersLink).click();
+        mainPage.newslettersLink.click();
         // Page "Newsletters" is opened 
+        const newslettersPage = new NewslettersPage();
         cy.url().should('contain', assertions.newslettersUrl);
         // Step 3
         // Check if following newsletters and their data are present on the page
         // All mentioned newsletters are present on the page and their data is matching 
         newsletters.forEach(newsletter => {
-            cy.get(euronews.newsletters).contains(newsletter.name).should('be.visible');
-            cy.get(euronews.newsletters).each(($el, index, $list) => {
+            newslettersPage.newsletters.contains(newsletter.name).should('be.visible');
+            newslettersPage.newsletters.each(($el, index, $list) => {
                 const name = $el.text();
                 if (name.includes(newsletter.name)) {
-                    expect($el.find(euronews.frequencyText).text()).to.contain(newsletter.frequency);
-                    expect($el.find(euronews.descriptionText).text()).to.contain(newsletter.description);
+                    expect($el.find(euronews.frequency).text()).to.contain(newsletter.frequency);
+                    expect($el.find(euronews.description).text()).to.contain(newsletter.description);
                 }
             });
         });
@@ -84,36 +94,37 @@ describe('Euronews tests', () => {
         // Click "Select this newsletter" on any newsletter
         cy.selectNewsletter(testdata.newsletters[0]);
         // Create account pop-up is pinned. 
-        cy.get(euronews.createAccountPopup).should('have.class', assertions.popupClass);
+        newslettersPage.createAccountPopup.should('have.class', assertions.popupClass);
         // Step 5
         // Click “Chosen”
-        cy.get(euronews.newsletters).each(($el, index, $list) => {
+        newslettersPage.newsletters.each(($el, index, $list) => {
             if ($el.find(euronews.newsletterText).text().includes(testdata.newsletters[0])) {
                 cy.wrap($el.find(euronews.chosenButton)).click();
             }
         });
         // Create account pop-up has unpinned
-        cy.get(euronews.createAccountPopup).should('not.have.class', assertions.popupClass);
+        newslettersPage.createAccountPopup.should('not.have.class', assertions.popupClass);
         // Step 6
         // Repeat step 4 
         cy.selectNewsletter(testdata.newsletters[1]);
         // Create account pop-up is pinned. 
-        cy.get(euronews.createAccountPopup).should('have.class', assertions.popupClass);
+        newslettersPage.createAccountPopup.should('have.class', assertions.popupClass);
         // Step 7
         // Type random email in create account pop-up. Click “Continue” 
-        const randomEmail = utils.generateRandomString(5) + testdata.domain;
-        cy.get(euronews.emailFieldPopup).type(randomEmail);
-        cy.get(euronews.continueButton).click();
+        const randomEmail = utils.generateRandomString(10) + testdata.domain;
+        newslettersPage.emailField.type(randomEmail);
+        newslettersPage.continueButton.click();
         // Complete registration page is opened
+        const registrationPage = new RegistrationPage();
         cy.url().should('contain', assertions.registerUrl);
         // The email field is automatically filled with the correct email
-        cy.get(euronews.emailField).should('have.text', randomEmail);
+        registrationPage.emailField.should('have.text', randomEmail);
         // Step 8
         // Enter random password and click “Create my account”
         const randomPassword = utils.generateRandomString(10);
-        cy.get(euronews.passwordField).type(randomPassword);
-        cy.get(euronews.createAccountButton).click();
+        registrationPage.passwordField.type(randomPassword);
+        registrationPage.createAccountButton.click();
         // Thank you message is displayed
-        cy.contains(euronews.confirmationMessage).should('be.visible');
+        registrationPage.confirmationMessage.should('be.visible');
     });
 });
---
+++ cypress/pageobjects/euronews/mainPage.js
@@ -0,0 +1,42 @@
+/// <reference types="Cypress" />
+import { euronews } from "../../support/locators";
+
+class MainPage {
+    get agreeCookiesButton() {
+        return cy.get(euronews.agreeCookiesButton);
+    }
+
+    get topTags() {
+        return cy.get(euronews.topTags);
+    }
+
+    get views() {
+        return cy.get(euronews.views);
+    }
+
+    get viewTitles() {
+        return cy.get(euronews.viewTitles);
+    }
+
+    get viewTitle() {
+        return cy.get(euronews.viewTitle);
+    }
+
+    get programsButton() {
+        return cy.get(euronews.programsButton);
+    }
+
+    get programTitles() {
+        return cy.get(euronews.programsTitles);
+    }
+
+    get topic() {
+        return cy.contains(euronews.topic);
+    }
+
+    get newslettersLink() {
+        return cy.contains(euronews.newslettersLink);
+    }
+}
+
+export default MainPage;
